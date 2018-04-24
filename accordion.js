be2163c5c602da508835d66b499c5ec0df59b36e
Colin Nico Schmid <mail@mala23.me>
2018-04-24 21:59:47 +0200

    write accordion script

---
+++ accordion.js
@@ -0,0 +1,26 @@
+function initAccordion(accordionElement) {
+
+  function handlePanelClick(event) {
+    showPanel(event.currentTarget)
+  }
+
+  function showPanel(panel) {
+    var expandedPanel = accordionElement.querySelector(".accordion__panel--status-active")
+    if (expandedPanel) {
+      expandedPanel.classList.remove(".accordion__panel--state-active")
+      expandedPanel.classList.add(".accordion__panel--state-inactive")
+    }
+    panel.classList.remove(".accordion__panel--state-inactive")
+    panel.classList.add(".accordion__panel--state-active")
+  }
+
+  var allPanelElements = accordionElement.querySelectorAll(".accordion__panel")
+  
+  for (var i = 0; i < allPanelElements.length; i++) {
+    allPanelElements[i].addEventListener("click", handlePanelClick)
+  }
+
+  showPanel(allPanelElements[0])
+}
+
+initAccordion(document.getElementById("accordion"))
